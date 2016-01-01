bb389d2a53fd49887ac030768ecceba833444e53
yell0wc0w <dark_shadex@hotmail.com>
2016-01-01 12:50:13 -0500

    Preparing for Heroku running

--- mysite/settings.py
+++ mysite/settings.py
@@ -118,7 +118,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+# BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 
